69f2cc52848d1b125faff3c58eb10135cf95073d
voidSatisfaction <lourie@naver.com>
2017-12-01 15:11:33 +0900

    feat: apply glosbe search command

--- services/botService/actionTreater.go
+++ services/botService/actionTreater.go
@@ -1,9 +1,9 @@
 package botService
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
-	"net/http"
+	"omt-project/api"
 )
 
 type ActionResult struct {
@@ -12,29 +12,70 @@ type ActionResult struct {
 
 // TreatAction is manager of actions
 func TreatAction(a *Action) *ActionResult {
-	ar := &ActionResult{}
+	actionResult := &ActionResult{}
 	switch a.actionType {
 	case Invalid:
-		InvalidActionTreat(a)
+		actionResult = TreatInvalidAction(a)
+	case InvalidCommand:
+		actionResult = TreatInvalidAction(a)
 	case Search:
 		// Call word search api
-		resp, err := http.Get("https://glosbe.com/gapi/translate?from=eng&dest=kor&format=json&pretty=true&phrase=dragon")
-		if err != nil {
-			ar.ServerError()
-			return ar
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		fmt.Printf("%+v", string(body))
-		ar.Text = "search"
+		actionResult = TreatSearchAction(a)
 	}
+	return actionResult
+}
+
+func TreatSearchAction(a *Action) *ActionResult {
+	ar := &ActionResult{}
+
+	// TODO: for korean user, use korean
+	glosbeParameter := &api.GlosbeParameter{
+		LanguageFrom: "eng",
+		LanguageTo:   "kor",
+		Phrase:       a.payloads[0],
+	}
+
+	glosbeClient := api.CreateGlosbeClient()
+	glosbeRequest, err := api.CreateGlosbeTranslateRequest(glosbeParameter)
+	if err != nil {
+		ar.ServerError()
+		return ar
+	}
+
+	resp, err := glosbeClient.Do(glosbeRequest)
+	if err != nil {
+		ar.ServerError()
+		return ar
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	gRes := api.GlosbeResponse{}
+	json.Unmarshal(body, &gRes)
+
+	// TODO: more elegant way
+	ar.Text = gRes.Tucs[0].Phrase.Text
 	return ar
 }
 
-func InvalidActionTreat(a *Action) {
-
+func TreatInvalidAction(a *Action) *ActionResult {
+	ar := &ActionResult{}
+	bp := BotPhrase{}
+	bp.Setting()
+	ar.Text = bp[a.actionType]
+	return ar
 }
 
-func (ar *ActionResult) ServerError() {
+func TreatInvalidCommandAction(a *Action) *ActionResult {
+	ar := &ActionResult{}
+	bp := BotPhrase{}
+	bp.Setting()
+	ar.Text = bp[a.actionType]
+	return ar
+}
+
+func (ar *ActionResult) ServerError() *ActionResult {
 	ar.Text = "先生、今、体の調子が悪いの"
+	return ar
 }
