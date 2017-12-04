874c88cab10b0a29b367cf2eb8f7044a9bf991a2
voidSatisfaction <lourie@naver.com>
2017-12-04 14:53:25 +0900

    test: add invalid and invalidCommand and predefinedAction

--- services/botService/actionTreater_test.go
+++ services/botService/actionTreater_test.go
@@ -4,17 +4,60 @@ import (
 	"testing"
 )
 
-func TestTreatAction(t *testing.T) {
-	expect := "용"
+func createInvalidDummyAction() *Action {
+	return &Action{
+		replyToken: "123",
+		actionType: Invalid,
+		payloads:   []string{},
+	}
+}
+
+func createInvalidCommandDummyAction() *Action {
+	return &Action{
+		replyToken: "123",
+		actionType: InvalidCommand,
+		payloads:   []string{},
+	}
+}
+
+func TestTreatSearchActionSuccess(t *testing.T) {
 	dummyAction := &Action{
 		replyToken: "123",
 		actionType: Search,
-		payloads:   []string{"dragon"},
+		payloads:   []string{"water"},
 	}
 
 	actionResult := TreatAction(dummyAction)
 	actual := actionResult.Text
-	if expect != actual {
-		t.Error("Expect search got ", actual)
+	if len(actual) <= 0 {
+		t.Error("Expect string size > 0, got ", len(actual))
+	}
+}
+
+func TestTreatPredefinedAction(t *testing.T) {
+	bp := BotPhrase{}
+	bp.Setting()
+
+	var tests = []struct {
+		action *Action
+		expect string
+	}{
+		{
+			createInvalidDummyAction(),
+			bp[Invalid],
+		},
+		{
+			createInvalidCommandDummyAction(),
+			bp[InvalidCommand],
+		},
+	}
+
+	for _, test := range tests {
+		actionResult := TreatAction(test.action)
+		expect := test.expect
+		actual := actionResult.Text
+		if expect != actual {
+			t.Errorf("Expect %s, got %s", expect, actual)
+		}
 	}
 }
