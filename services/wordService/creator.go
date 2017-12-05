8e1da98a38f77b8cb2b73cc2c272b3ec56ec8e02
voidSatisfaction <lourie@naver.com>
2017-12-05 18:28:49 +0900

    [WIP] add Add word and Read word

--- services/wordService/creator.go
+++ services/wordService/creator.go
@@ -3,7 +3,7 @@ package wordService
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
 	"omt-project/api/awsS3"
 	"omt-project/config"
 
@@ -12,40 +12,111 @@ import (
 )
 
 type WordsInfo struct {
-	Words []Word
+	Words Words `json:"words"`
 }
+
+type Words []Word
 
 type Word struct {
-	Name     string
-	Meaning  []string
-	Priority int
+	Name     string   `json:"name"`
+	Meaning  []string `json:"meaning"`
+	Priority int      `json:"priority"`
 }
 
-func CreateNewWord(userId string) {
+func CreateNewWord(userId string) error {
 	c := config.Setting()
 	svc, err := awsS3.CreateS3Client()
 	if err != nil {
-		return
+		return err
 	}
 
 	wordsInfo := WordsInfo{
-		Words: []Word{},
+		Words: Words{},
 	}
 
 	wordsInfoJson, err := json.Marshal(wordsInfo)
 	if err != nil {
-		return
+		return err
 	}
 
-	wordKey := fmt.Sprintf("%s%s", c.AwsS3BucketWordsKey, userId)
+	wordKey := awsS3.GetWordKey(userId)
 	_, err = svc.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(c.AwsS3Bucket),
 		Key:    aws.String(wordKey),
 		// TODO: casting directly []byte() is not efficient refer: https://qiita.com/ikawaha/items/3c3994559dfeffb9f8c9
 		Body: bytes.NewReader(wordsInfoJson),
 	})
-
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	return nil
+}
+
+func ReadWord(userId string) (*WordsInfo, error) {
+	c := config.Setting()
+	svc, err := awsS3.CreateS3Client()
+	if err != nil {
+		return nil, err
+	}
+
+	wordKey := awsS3.GetWordKey(userId)
+	res, err := svc.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(c.AwsS3Bucket),
+		Key:    aws.String(wordKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	wi := &WordsInfo{}
+	if err := json.Unmarshal(body, wi); err != nil {
+		return nil, err
+	}
+
+	return wi, nil
+}
+
+func Addword(userId string, wordName string, meaning []string) error {
+	c := config.Setting()
+	svc, err := awsS3.CreateS3Client()
+	if err != nil {
+		return err
+	}
+
+	wordsInfo, err := ReadWord(userId)
+	if err != nil {
+		return err
+	}
+
+	w := Word{
+		Name:     wordName,
+		Meaning:  meaning,
+		Priority: 100,
+	}
+	wordsInfo.Words = append(wordsInfo.Words, w)
+
+	wordsInfoJSON, err := json.Marshal(wordsInfo)
+	if err != nil {
+		return err
+	}
+
+	wordKey := awsS3.GetWordKey(userId)
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(c.AwsS3Bucket),
+		Key:    aws.String(wordKey),
+		// TODO: casting directly []byte() is not efficient refer: https://qiita.com/ikawaha/items/3c3994559dfeffb9f8c9
+		Body: bytes.NewReader(wordsInfoJSON),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- services/wordService/creator_test.go
+++ services/wordService/creator_test.go
@@ -6,3 +6,18 @@ func TestCreateNewWord(t *testing.T) {
 	userId := "abc"
 	CreateNewWord(userId)
 }
+
+func TestReadWord(t *testing.T) {
+	userId := "abc"
+	ReadWord(userId)
+}
+
+func TestAddWord(t *testing.T) {
+	userId := "abc"
+	wordName := "water"
+	meaning := []string{"水", "みず"}
+	err := Addword(userId, wordName, meaning)
+	if err != nil {
+		t.Errorf("There was error: %+v", err)
+	}
+}
